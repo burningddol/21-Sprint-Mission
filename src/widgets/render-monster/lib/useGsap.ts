1160f7cdd5e2c7d00ea0489d9e5510c8e3737fee
junhub98 <helpwoodjs@icloud.com>
2026-01-12 03:39:11 +0900

    change: evenly distributed monster's Vector3 position

--- src/widgets/render-monster/lib/useGsap.ts
+++ src/widgets/render-monster/lib/useGsap.ts
@@ -24,28 +24,54 @@ useGsap.changeRotation = (rotation: THREE.Euler) => {
   });
 };
 
-const MIN = -50;
-const MAX = 50;
-const N = 44;
+function getXbyIndex(
+  index: number,
+  inMin: number,
+  inMax: number,
+  outMin: number,
+  outMax: number
+) {
+  return outMin + ((index - inMin) / (inMax - inMin)) * (outMax - outMin);
+}
+
+function getZbyX(
+  x: number,
+  falloffRange: number, // 예: 50  → 이 거리에서 거의 죽음
+  minZ: number, // 예: -70
+  maxZ: number, // 예: 35
+  sharpness = 3.5 // 예: 3.5 (클수록 더 빨리 -70로 떨어짐)
+) {
+  const d = Math.abs(x) / falloffRange; // 0 ~ ∞
+  const b = Math.exp(-d * d * sharpness); // 1 → 0
+  return minZ + b * (maxZ - minZ);
+}
+
+function getInitYbyZ(v: number, outMin: number, outMax: number) {
+  // input range is fixed: -70 ~ 36
+  const t = (v + 70) / 106; // 100 = 36 - (-70)
+  return outMin + t * (outMax - outMin);
+}
 
 useGsap.changePosition = (position: THREE.Vector3, index: number) => {
-  const t = index / (N - 1);
-  const u = 0.5 - 0.5 * Math.cos(t * Math.PI);
-  const x = MIN + u * (MAX - MIN);
+  const x = getXbyIndex(index, 0, 43, -110, 110);
+
+  const z = getZbyX(x, 60, -70, 36, 3.5);
+
+  const initY = getInitYbyZ(z, -70, -18);
 
   position.x = x;
   position.y = Math.random() * 85 - 50;
-  position.z = Math.random() * 107 - 70;
+  position.z = z;
   const durationTime = Math.random() * 25 + 15;
   gsap.to(position, {
-    y: 55,
+    y: -initY,
 
     duration: durationTime,
     repeat: -1,
     ease: "none",
 
     onRepeat(this: gsap.core.Tween) {
-      position.y = -55;
+      position.y = initY;
       this.invalidate().restart();
     },
   });
