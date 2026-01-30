b90b518fcf18387a1c5d057be469c67703050cd7
junhub98 <helpwoodjs@icloud.com>
2026-01-30 21:48:38 +0900

    refactor: auth section

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,14 +1,10 @@
-import { useCallback, useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-import { useUser } from '../common/UserProvider';
-import useOutsideClick from '@/hooks/useOutsideClick';
-import Button from '../common/Button';
+import AuthSection from '../common/AuthSection';
 
 import pandaTitle from '@/assets/panda_title.png';
 import titleImg from '@/assets/title_productPage.png';
-import userIcon from '../../assets/user_icon.png';
 import media from '../../utils/media';
 import styles from './Nav.module.scss';
 
@@ -71,80 +67,7 @@ const StyledNavLink = styled(NavLink)`
   `}
 `;
 
-const ProfileButton = styled.button`
-  position: relative;
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-`;
-
-const DropdownMenu = styled.div`
-  position: absolute;
-  right: 0;
-  top: 45px;
-  width: 139px;
-  height: 51px;
-  background-color: var(--white);
-  border-radius: 8px;
-  border: 1px solid var(--gray-300);
-  font-family: 'pretendard';
-  font-weight: 400;
-  font-size: 16px;
-  color: var(--gray-500);
-  align-content: center;
-  cursor: pointer;
-`;
-
 export default function Nav() {
-  const { user, setUser } = useUser();
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  const closeDropdown = useCallback(() => {
-    setIsDropdownOpen(false);
-  }, []);
-
-  const dropdownRef = useOutsideClick(closeDropdown, isDropdownOpen);
-
-  const handleLogout = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
-    setUser(null);
-  };
-
-  const renderAuthArea = () => {
-    if (user === 'isPending') {
-      return <span>로딩중...</span>;
-    }
-
-    if (user) {
-      return (
-        <ProfileButton onClick={() => setIsDropdownOpen((prev) => !prev)}>
-          <img src={userIcon} alt="유저 아이콘" />
-          {isDropdownOpen && (
-            <DropdownMenu onClick={handleLogout} ref={dropdownRef}>
-              로그아웃
-            </DropdownMenu>
-          )}
-        </ProfileButton>
-      );
-    }
-
-    return (
-      <Link to="/login">
-        <Button
-          width="128px"
-          height="48px"
-          $borderRadius="8px"
-          fontSize="16px"
-          fontWeight="400"
-        >
-          로그인
-        </Button>
-      </Link>
-    );
-  };
-
   return (
     <NavContainer>
       <div className={styles.left}>
@@ -154,7 +77,7 @@ export default function Nav() {
         <StyledNavLink to="/boards">자유게시판</StyledNavLink>
         <StyledNavLink to="/products">중고마켓</StyledNavLink>
       </div>
-      {renderAuthArea()}
+      <AuthSection />
     </NavContainer>
   );
 }
--- src/components/main/MainNav.tsx
+++ src/components/main/MainNav.tsx
@@ -1,14 +1,11 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+import AuthSection from '../common/AuthSection';
+
 import pandaTitle from '@/assets/panda_title.png';
 import titleImg from '@/assets/onlyTitle.png';
-import userIcon from '../../assets/user_icon.png';
 import media from '../../utils/media';
-import Button from '../common/Button';
-import { useUser } from '../common/UserProvider';
-import useOutsideClick from '@/hooks/useOutsideClick';
 
 const NavBox = styled.div`
   width: 100%;
@@ -42,87 +39,14 @@ const TitleLogo = styled.div`
   `}
 `;
 
-const ProfileButton = styled.button`
-  border: none;
-  background-color: transparent;
-  position: relative;
-  cursor: pointer;
-`;
-
-const DropdownMenu = styled.div`
-  position: absolute;
-  right: 0;
-  top: 45px;
-  width: 139px;
-  height: 51px;
-  background-color: var(--white);
-  border-radius: 8px;
-  border: 1px solid var(--gray-300);
-  font-family: 'pretendard';
-  font-weight: 400;
-  font-size: 16px;
-  color: var(--gray-500);
-  align-content: center;
-  cursor: pointer;
-`;
-
 export default function MainNav() {
-  const { user, setUser } = useUser();
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const onClose = useCallback(() => {
-    setIsDropdownOpen(false);
-  }, []);
-  const dropdownRef = useOutsideClick(onClose, isDropdownOpen);
-
-  const handleLogout = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
-    setUser(null);
-  };
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen((prev) => !prev);
-  };
-
-  const renderAuthSection = () => {
-    if (!user) {
-      return (
-        <Link to="/login">
-          <Button
-            width="128px"
-            height="48px"
-            $borderRadius="8px"
-            fontSize="16px"
-            fontWeight="400"
-          >
-            로그인
-          </Button>
-        </Link>
-      );
-    }
-
-    if (user === 'isPending') return <span>로딩중...</span>;
-
-    return (
-      <ProfileButton onClick={toggleDropdown}>
-        <img src={userIcon} alt="유저 프로필" />
-        {isDropdownOpen && (
-          <DropdownMenu onClick={handleLogout} ref={dropdownRef}>
-            로그아웃
-          </DropdownMenu>
-        )}
-      </ProfileButton>
-    );
-  };
-
   return (
     <header>
       <NavBox>
         <Link to="/">
           <TitleLogo />
         </Link>
-        {renderAuthSection()}
+        <AuthSection />
       </NavBox>
     </header>
   );
